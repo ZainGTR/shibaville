9e390371f8ba09b7c7fb2415af8e2bdb72b13a64
ZainGTR <askaliceai@gmail.com>
2024-06-13 10:11:58 +0400

    multi-token state

--- resources/contracts/resource/src/state.rs
+++ resources/contracts/resource/src/state.rs
@@ -1,13 +1,41 @@
-use schemars::JsonSchema;
+use cosmwasm_std::{Addr, Storage, Uint128, StdResult, StdError};
+use cw_storage_plus::{Item, Map};
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: Addr,
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+pub struct TokenInfo {
+    pub name: String,
+    pub symbol: String,
+    pub decimals: u8,
+    pub total_supply: Uint128,
 }
 
-pub const STATE: Item<State> = Item::new("state");
+pub const TOKENS: Map<&str, TokenInfo> = Map::new("tokens");
+pub const BALANCES: Map<(&Addr, &str), Uint128> = Map::new("balances");
+
+pub fn increase_balance(
+    storage: &mut dyn Storage,
+    owner: &Addr,
+    denom: &str,
+    amount: Uint128,
+) -> StdResult<()> {
+    let key = (owner, denom);
+    let balance = BALANCES.may_load(storage, key)?.unwrap_or_default();
+    BALANCES.save(storage, key, &(balance + amount))?;
+    Ok(())
+}
+
+pub fn decrease_balance(
+    storage: &mut dyn Storage,
+    owner: &Addr,
+    denom: &str,
+    amount: Uint128,
+) -> StdResult<()> {
+    let key = (owner, denom);
+    let balance = BALANCES.may_load(storage, key)?.unwrap_or_default();
+    if balance < amount {
+        return Err(StdError::generic_err("Insufficient funds"));
+    }
+    BALANCES.save(storage, key, &(balance - amount))?;
+    Ok(())
+}
