a88ed2bdb6504a7cf82040c0fcfdf2feb50a1a57
ZainGTR <askaliceai@gmail.com>
2024-06-13 05:50:22 +0400

    resources contract init

---
+++ resources/contracts/resource/src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use resource::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
