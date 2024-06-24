c1b8a2575bae4ba08c57578abc862ff179b0eb2b
ZainGTR <askaliceai@gmail.com>
2024-06-24 17:10:51 +0400

    archdemo init

---
+++ demo/dapp/archdemo/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex bg-slate-300">
+        <h1>TEST</h1>
+      </div>
+    </main>
+  );
+}
